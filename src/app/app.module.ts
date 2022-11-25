61206405eae909cd80a623191181120b8f7d2186
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-11-25 10:35:00 +0100

    feat: add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FloatLabelComponent } from './float-label/float-label.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FloatLabelComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
